22c46d5121b87c38f8b14d4d2be9692a652d616a
Nathaniel-AW-Amon <bossamon54@gmail.com>
2025-07-11 10:29:32 -0400

    Create GameTable.tsx

---
+++ GameTable.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GameTable = () => {
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h2>Game Table</h2>
+      <p>🎲 Dice rolls will appear here...</p>
+    </div>
+  );
+};
+
+export default GameTable;
